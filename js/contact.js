0c8ac40373cd345fbf0390d790632c7d4bfc2308
sandunrmst <sandunalugc@gmail.com>
2023-06-26 13:33:32 +0530

    Add Contact Form

---
+++ js/contact.js
@@ -0,0 +1,25 @@
+(function (){
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzmi8lY8eNwCngZzmI8nnIZtLxBSEsKQtrHLDxaAkNIc8_ICabnh08UYlXOTaMSO5p_HQ/exec';
+    const form = document.forms['submit-to-google-sheet'];
+
+    const msg = document.getElementById("sent-message");
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+          msg.innerHTML = "Message sent successfully! 😊";
+          setTimeout(function(){
+             msg.innerHTML = "";
+          }, 4000);
+          form.reset();
+        })
+        .catch(error => {
+          msg.innerHTML = "Sorry, Couldn't send message on this time! 😞";
+        });
+    });
+
+
+}());
+
