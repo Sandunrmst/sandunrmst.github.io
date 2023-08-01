279c2d208316b3e8b13c87cc9e079ccd292f94e1
sandunrmst <sandunalugc@gmail.com>
2023-08-01 14:02:05 +0530

    added preloader for message sending

--- js/contact.js
+++ js/contact.js
@@ -4,11 +4,17 @@
     const form = document.forms['submit-to-google-sheet'];
 
     const msg = document.getElementById("sent-message");
+
   
     form.addEventListener('submit', e => {
       e.preventDefault();
+
+      document.getElementById('message-loader').className = "show-message";
+      document.getElementById('message-loader').innerHTML = '<div class="preloader-sendMail"><div class="loading-dot"></div></div>';
+      
       fetch(sURL, { method: 'POST', body: new FormData(form)})
         .then(response => {
+          document.getElementById('message-loader').className = "hide-message";
           msg.innerHTML = "Message sent successfully! 😊";
           setTimeout(function(){
              msg.innerHTML = "";
@@ -16,6 +22,7 @@
           form.reset();
         })
         .catch(error => {
+          document.getElementById('message-loader').className = "hide-message";
           msg.innerHTML = "Sorry, Couldn't send message on this time! 😞";
         });
     });
