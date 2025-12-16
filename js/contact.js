65bf90b626e1919fafa90d98161edcb2c7d7bc9f
Sandun Tharuka <sandunrmst@gmail.com>
2025-12-16 21:01:06 +0530

    fix contact issues

--- js/contact.js
+++ js/contact.js
@@ -1,31 +1,31 @@
-(function (){
+(function () {
 
-    const sURL = 'https://script.google.com/macros/s/AKfycbzmi8lY8eNwCngZzmI8nnIZtLxBSEsKQtrHLDxaAkNIc8_ICabnh08UYlXOTaMSO5p_HQ/exec';
-    const form = document.forms['submit-to-google-sheet'];
+  const sURL = 'https://script.google.com/macros/s/AKfycbxOHUMcJVW9jgTh5uMDcY0ahGbJXJ6th7tvM_nCnKCKubzSTZ_aHumo1EKafyB0ywJSxQ/exec';
+  const form = document.forms['submit-to-google-sheet'];
 
-    const msg = document.getElementById("sent-message");
+  const msg = document.getElementById("sent-message");
 
-  
-    form.addEventListener('submit', e => {
-      e.preventDefault();
 
-      document.getElementById('message-loader').className = "show-message";
-      document.getElementById('message-loader').innerHTML = '<div class="preloader-sendMail"><div class="loading-dot"></div></div>';
-      
-      fetch(sURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-          document.getElementById('message-loader').className = "hide-message";
-          msg.innerHTML = "Message sent successfully! 😊";
-          setTimeout(function(){
-             msg.innerHTML = "";
-          }, 4000);
-          form.reset();
-        })
-        .catch(error => {
-          document.getElementById('message-loader').className = "hide-message";
-          msg.innerHTML = "Sorry, Couldn't send message on this time! 😞";
-        });
-    });
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    document.getElementById('message-loader').className = "show-message";
+    document.getElementById('message-loader').innerHTML = '<div class="preloader-sendMail"><div class="loading-dot"></div></div>';
+
+    fetch(sURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        document.getElementById('message-loader').className = "hide-message";
+        msg.innerHTML = "Message sent successfully! 😊";
+        setTimeout(function () {
+          msg.innerHTML = "";
+        }, 4000);
+        form.reset();
+      })
+      .catch(error => {
+        document.getElementById('message-loader').className = "hide-message";
+        msg.innerHTML = "Sorry, Couldn't send message on this time! 😞";
+      });
+  });
 
 
 }());
