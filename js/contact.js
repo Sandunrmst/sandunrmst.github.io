0f7774acb1b8cf5bca6dd0fc23c30ac72d544692
Sandun Tharuka <sandunrmst@gmail.com>
2025-12-17 11:35:12 +0530

    fix and update UI on contact

--- js/contact.js
+++ js/contact.js
@@ -1,37 +1,37 @@
 (function () {
 
-  const sURL = 'https://script.google.com/macros/s/AKfycbx-BEy24tR_6wV5xqqdMz5r4aTTkOQcRpv0hyh6kM6U4xgysbJlvOWojyAfTvbS_AiLPQ/exec';
+  const sURL = "https://script.google.com/macros/s/AKfycbz9x4rGiI-aGIISODJYa0EH6Hp1bVTHWZMZcuxZfjgx2eQFHnjG4kw-t2uucS_Yj_rx/exec";
   const form = document.forms['submit-to-google-sheet'];
-
   const msg = document.getElementById("sent-message");
-
+  const loader = document.getElementById("message-loader");
 
   form.addEventListener('submit', e => {
     e.preventDefault();
 
-    document.getElementById('message-loader').className = "show-message";
-    document.getElementById('message-loader').innerHTML = '<div class="preloader-sendMail"><div class="loading-dot"></div></div>';
+    loader.className = "show-message";
 
-    fetch(sURL, { method: 'POST', body: new FormData(form) })
+    fetch(sURL, {
+      method: 'POST',
+      body: new FormData(form)
+    })
       .then(res => res.json())
       .then(data => {
-        document.getElementById('message-loader').className = "hide-message";
+        loader.className = "hide-message";
 
         if (data.result === "success") {
           msg.innerHTML = "Message sent successfully! 😊";
           form.reset();
         } else {
-          throw new Error("Script error");
+          throw new Error("Server error");
         }
 
-        setTimeout(() => msg.innerHTML = "", 2000);
+        setTimeout(() => msg.innerHTML = "", 3000);
       })
-      .catch(() => {
-        document.getElementById('message-loader').className = "hide-message";
+      .catch(err => {
+        loader.className = "hide-message";
         msg.innerHTML = "Sorry, couldn't send message 😞";
       });
   });
-
 
 }());
 
